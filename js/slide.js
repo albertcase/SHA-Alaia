d76a0a30f5e51ac333ce7311e644995528129c1e
evelynzhang88 <1054982653@qq.com>
2016-02-04 15:31:46 +0800

    slide image

---
+++ js/slide.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $("#slides-1").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-2").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-3").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-4").slidesjs({
+        width: 789,
+        height: 453
+    });
+
+    $("#slides-5").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-6").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-7").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-8").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+    $("#slides-9").slidesjs({
+        width: 789,
+        height: 546
+    });
+
+});
