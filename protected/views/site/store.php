0b7e7fb83ef4cb52b6265a08baeb77bff93d8c79
evelynzhang88 <1054982653@qq.com>
2016-02-18 17:30:30 +0800

    the store page

---
+++ protected/views/site/store.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>阿莱亚ALAIA-店铺显示</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" >
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" type="text/css" href="/html/css/screen.css?v=1.0"/>
+</head>
+<body class="page-store">
+<div class="avatar">
+    <img src="/img/avatar.png" alt=""/>
+</div>
+<div class="add-content">
+    <h3 class="store-name">阿莱亚MARIGNAN店</h3>
+    <hr>
+    <div class="store-address">地址：5 rue de Marignan, 75008 Paris</div>
+    <div class="store-phone">电话：01 76 72 91 11</div>
+    <div class="store-mail">邮箱： boutique.marignan@alaia.fr </div>
+    <div class="store-time">营业时间：周一至周六10点至19点</div>
+</div>
+<div class="store-map">
+    <img src="/img/avatar.png" alt=""/>
+</div>
+</body>
+</html>
