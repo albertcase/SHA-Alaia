6ae0bdfac3f825ef753cb88204cbee71a3de41fc
Dirc Wang <dirk.wang@samesamechina.com>
2016-02-22 15:38:58 +0800

    upload gitignore2

--- protected/config/main.php
+++ protected/config/main.php
@@ -77,7 +77,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=alaia_wechat',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '123456',
+			'password' => '1qazxsw2',
 			'charset' => 'utf8',
 		),
 		
