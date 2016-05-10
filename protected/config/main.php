c5a26e489ab57bbb8433a8fa1ae72d83ea3df2a4
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-10 18:50:06 +0800

    change store page3

--- protected/config/main.php
+++ protected/config/main.php
@@ -77,7 +77,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=alaia_wechat',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '123456',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 
